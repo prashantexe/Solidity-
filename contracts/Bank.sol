2bd220ee64f1e68dc3f6ae4c36252d3b18b2467e
prashantexe <prashantd049@gmail.com>
2023-03-13 21:47:34 +0530

    Commit

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -15,3 +15,11 @@ contract Bank {
     function deposit() external payable {
         balanceOf[msg.sender] += msg.value;
     }
+
+    /* 3. attach nonReentrant to protect against reentracy */
+    function withdraw() external {
+        uint256 depositedAmount = balanceOf[msg.sender];
+        payable(msg.sender).sendValue(depositedAmount);
+        balanceOf[msg.sender] = 0;
+    }
+}
